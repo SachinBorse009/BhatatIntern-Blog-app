614363b20e7ae7bce6f1ba14ccd261e907461b97
Sachin Borse <111965224+SachinBorse009@users.noreply.github.com>
2024-01-13 20:04:00 +0530

    First Commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+
+const Footer = () => {
+  return (
+    <>
+<div className="mt-8 w-full bg-green-500 px-8 md:px-[300px] md:space-y-0 items-start md:justify-between text-sm md:text-md py-8 ">
+       
+
+       <div className="flex flex-col md:flex-row md:justify-around  text-white">
+         <p>Privacy Policy</p>
+         <p>About Us</p>
+         <p>Terms & Conditions</p>
+         <p>Terms of Service</p>
+       </div>
+    </div>
+    <p className="py-2 pb-6 text-center text-white bg-black text-sm">All rights reserved @Blog Explorer 2024</p>
+    </>
+    
+  )
+}
+
+export default Footer
